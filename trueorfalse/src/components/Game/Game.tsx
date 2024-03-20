bc705a8e5f1463fd7bcfcb6036d200303d7e05f1
Sandy Pirôpo <121200148+sandypiropo@users.noreply.github.com>
2024-03-20 20:54:00 -0300

    Game fold

---
+++ trueorfalse/src/components/Game/Game.tsx
@@ -0,0 +1,13 @@
+// GamePage.tsx
+
+import React from 'react';
+
+const GamePage: React.FC = () => {
+  return (
+    <div>
+      {/* Conteúdo da página de jogo */}
+    </div>
+  );
+}
+
+export default GamePage;
