68be16b1521a7873a2567d72dc12681cf43899d4
Sandy Pirôpo <121200148+sandypiropo@users.noreply.github.com>
2024-03-23 15:53:53 -0300

    Pasta index.css limpa

--- trueorfalse/src/components/Game/Game.tsx
+++ trueorfalse/src/components/Game/Game.tsx
@@ -1,11 +1,19 @@
 // GamePage.tsx
-
+import './Game.css'
 import React from 'react';
 
 const GamePage: React.FC = () => {
   return (
-    <div>
-      <h1>JOGANDOOOOOOOOOOOOOOOO</h1>
+    <div className='game-page'>
+      
+      <div className='left-side'>
+        <h1>JOGANDOOOOOOOOOOOOOOOO</h1>
+      </div>
+
+      <div className='right-side'>
+        <h1>IMAGEMMMMMMMMMM</h1>
+      </div>
+
     </div>
   );
 }
