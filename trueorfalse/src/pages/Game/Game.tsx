68a70f9c22f1c95607a9c894a61a95ddc25e61b5
Sandy Pirôpo <121200148+sandypiropo@users.noreply.github.com>
2024-03-24 02:31:22 -0300

    prettier

--- trueorfalse/src/pages/Game/Game.tsx
+++ trueorfalse/src/pages/Game/Game.tsx
@@ -1,23 +1,21 @@
 // GamePage.tsx
-import './Game.css';
-import Button from '../../components/buttonhome/ButtonHome';
-import React from 'react';
-
+import "./Game.css";
+import React from "react";
 
 const GamePage: React.FC = () => {
   return (
-      <div className='game-bg'>
-        
-        <div className='left-side'>
+    <div className="game-bg">
+      <div className="game-content">
+        <div className="left-side">
           <h1>JOGANDOOOOOOOOOOOOOOOO</h1>
         </div>
 
-        <div className='right-side'>
+        <div className="right-side">
           <h1>IMAGEMMMMMMMMMM</h1>
         </div>
-        <Button/>
       </div>
+    </div>
   );
-}
+};
 
 export default GamePage;
